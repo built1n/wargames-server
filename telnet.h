e3e5b740adfa35202b5559e8867b8edb6e08ff1b
Franklin Wei <frankhwei536@gmail.com>
2014-07-16 11:07:37 -0400

    Added missing file

---
+++ telnet.h
@@ -0,0 +1,23 @@
+#define LF 10
+#define CR 13
+#define BEL 7
+#define BS 8
+#define FF 12
+#define SE 240
+#define NOP 241
+#define AYT 246
+#define GA 249
+
+#define SB 250
+#define WILL 251
+#define WONT 252
+#define DO 253
+#define DONT 254
+#define IAC 255
+
+#define SGA 3
+#define STATUS 5
+#define ECHO 1
+#define TERMTYPE 24
+#define NAWS 31
+#define SURPRESS_LOCAL_ECHO 0x2D
